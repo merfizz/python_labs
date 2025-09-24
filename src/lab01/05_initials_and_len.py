3f5deca8fd2a9d028b11eeeab6e03b26fcd14bf9
merfizz <katz.ignatoval@gmail.com>
2025-09-24 16:18:58 +0300

    lab

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -1,26 +1,9 @@
-"""
+
 fio=str(input("ФИО: "))
-ini=''
-len_=0
-i=1
-for i in range (len(fio)):
-    if (ord(fio[i])>=1040)and(ord(fio[i])<=1071) : 
-       ini= ini + fio[i]
-    if fio[i]!=" ":
-        len_+=1
-
-
-
-print(f"Инициалы: {ini}.\nДлина (символов): {len_+2} ")
-"""
-fio=str(input("ФИО: "))
-ini=''
-len_=0
-
 new_fio=fio.split()
-
-res=f"{new_fio[0][0]}{new_fio[1][0]}{new_fio[2][0]} "
-
-print(f"{res}")
+fio_new=f"{new_fio[0]} {new_fio[1]} {new_fio[2]}"
+res=f"{new_fio[0][0]}{new_fio[1][0]}{new_fio[2][0]}."
+len=len(fio_new)
+print(f"Инициалы: {res} \nДлина:{len}")
 
 
