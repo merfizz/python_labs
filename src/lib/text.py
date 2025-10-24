f6f6ae2af6a1b94e3642b58268ad214ce2988fdb
merfizz <katz.ignatoval@gmail.com>
2025-10-24 11:31:46 +0300

    03

--- src/lib/text.py
+++ src/lib/text.py
@@ -1,6 +1,7 @@
 import re
 
 def normalize(text: str, *, casefold: bool = True, yo2e: bool = True) -> str:
+    """Функция приводит текст к единому регистру и стилю"""
     s=text
     if casefold :
         s=s.casefold()
@@ -16,18 +17,21 @@ def normalize(text: str, *, casefold: bool = True, yo2e: bool = True) -> str:
     return s
 
 def tokenize(text: str) -> list[str]:
+    """Функция разбивает текст на «слова» по небуквенно-цифровым разделителям"""
     pattern = r'[\w-]+'
     tokenstext = re.findall(pattern, text)
 
     return tokenstext
 
 def count_freq(tokens: list[str]) -> dict[str, int]:
+    """Функция возвращает словарь слово - количество"""
     counts={}
     for word in tokens:
         counts[word]=counts.get(word,0)+1
     return counts
 
 def top_n(freq: dict[str, int], n: int = 5) -> list[tuple[str, int]]:
+    """Функция возвращает топ-N по убыванию частоты"""
     sorted_freq= sorted(freq.items(),key=lambda item: [-item[1], item[0]])
     top_n=[]
 
@@ -35,5 +39,3 @@ def top_n(freq: dict[str, int], n: int = 5) -> list[tuple[str, int]]:
         top_n.append((sorted_freq[i][0], sorted_freq[i][1]))
 
     return top_n
-    
-
